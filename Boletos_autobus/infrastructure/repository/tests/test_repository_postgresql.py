bedf831cbc1c2e377277ab679bb6f5caefcc8e1f
victor ortega <vorteg.r@gmail.com>
2024-06-11 16:22:00 -0600

    Agregando unit test para verificar que existen tablas en db

--- Boletos_autobus/infrastructure/repository/tests/test_repository_postgresql.py
+++ Boletos_autobus/infrastructure/repository/tests/test_repository_postgresql.py
@@ -1,9 +1,10 @@
 import logging
 import pytest
-from sqlalchemy import text
+from sqlalchemy import text,inspect
 
 logger = logging.getLogger(__name__)
 
+#Smoke: Test DB Connection
 @pytest.mark.skip(reason="La conexión a la base de datos ha sido verificada.")
 def test_db_connection(db_session):
     result = db_session.execute(text('SELECT version();'))
@@ -11,3 +12,10 @@ def test_db_connection(db_session):
     logger.info("Hola desde Tests")
     logger.info(f"\n Esta es la version: {version_info[0]}")
     assert result is not None, "La conexión a la base de datos falló"
+    
+def test_create_tables(db_session):
+    inspector = inspect(db_session.bind)
+    tables = inspector.get_table_names()
+    assert 'usuarios' in tables, "La tabla 'usuarios' no existe en la base de datos"
+    assert 'asientos' in tables, "La tabla 'asientos' no existe en la base de datos"
+    assert 'boletos' in tables, "La tabla 'boletos' no existe en la base de datos"
